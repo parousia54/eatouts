a0f1c34c6474b7f597b79ad18d45b597acbd07fd
rajeshraman <rajesh@jmango.net>
2013-08-06 00:07:17 +1000

    First commit -
    - Map shown to point to current location with address displayed.

---
+++ src/com/parousia/eatouts/MapActivity.java
@@ -0,0 +1,269 @@
+package com.parousia.eatouts;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapActivity extends FragmentActivity implements
+		ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+
+	private static final long MIN_TIME = 400;
+	private static final float MIN_DISTANCE = 1000;
+	private LocationRequest mLocationRequest;
+	private LocationClient mLocationClient;
+	private GoogleMap mMap;
+	private LocationManager locationManager;
+	private TextView mAddressBar;
+
+	@Override
+	protected void onCreate(Bundle saveInstanceState) {
+
+		super.onCreate(saveInstanceState);
+		setContentView(R.layout.activity_map);
+
+		// Create a new global location parameters object
+		mLocationRequest = LocationRequest.create();
+
+		/*
+		 * Set the update interval
+		 */
+		mLocationRequest
+				.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
+
+		// Use high accuracy
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+		// Set the interval ceiling to one minute
+		mLocationRequest
+				.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+		
+		mAddressBar = (TextView)findViewById(R.id.addressbar);
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+		setUpLocationClientIfNeeded();
+		mLocationClient.connect();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (mLocationClient != null) {
+			mLocationClient.disconnect();
+		}
+	}
+
+	private void setUpLocationClientIfNeeded() {
+		if (mLocationClient == null) {
+			mLocationClient = new LocationClient(getApplicationContext(), this,
+					this);
+		}
+	}
+
+	private void setUpMapIfNeeded() {
+		if (mMap == null) {
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			if (mMap != null) {
+				mMap.setMyLocationEnabled(true);
+			}
+		}
+	}
+
+	public void showMyLocation(View view) {
+		if (mLocationClient != null && mLocationClient.isConnected()) {
+			String msg = "Location = " + mLocationClient.getLastLocation();
+			Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		Log.d(LocationUtils.APPTAG,
+				"Loaction Changed -" + location.getLatitude() + ", "
+						+ location.getLongitude());
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+				.target(new LatLng(location.getLatitude(), location.getLongitude())) // Sets the
+				// center of
+				// the map
+				// to
+				// Mountain View
+				.zoom(17) // Sets the zoom
+				.bearing(90) // Sets the orientation of the camera to
+								// east
+				.tilt(30) // Sets the tilt of the camera to 30 degrees
+				.build(); // Creates a CameraPosition from the builder
+		mMap.animateCamera(CameraUpdateFactory
+				.newCameraPosition(cameraPosition));
+		
+		(new MapActivity.GetAddressTask(this)).execute(location);
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// Do nothing
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		mLocationClient.requestLocationUpdates(mLocationRequest, this);
+	}
+
+	@Override
+	public void onDisconnected() {
+		// Do nothing
+	}
+	
+	
+	protected class GetAddressTask extends AsyncTask<Location, Void, String> {
+
+		// Store the context passed to the AsyncTask when the system
+		// instantiates it.
+		Context localContext;
+
+		// Constructor called by the system to instantiate the task
+		public GetAddressTask(Context context) {
+
+			// Required by the semantics of AsyncTask
+			super();
+
+			// Set a Context for the background task
+			localContext = context;
+		}
+
+		/**
+		 * Get a geocoding service instance, pass latitude and longitude to it,
+		 * format the returned address, and return the address to the UI thread.
+		 */
+		@Override
+		protected String doInBackground(Location... params) {
+			/*
+			 * Get a new geocoding service instance, set for localized
+			 * addresses. This example uses android.location.Geocoder, but other
+			 * geocoders that conform to address standards can also be used.
+			 */
+			Geocoder geocoder = new Geocoder(localContext, Locale.getDefault());
+
+			// Get the current location from the input parameter list
+			Location location = params[0];
+
+			// Create a list to contain the result address
+			List<Address> addresses = null;
+
+			// Try to get an address for the current location. Catch IO or
+			// network problems.
+			try {
+
+				/*
+				 * Call the synchronous getFromLocation() method with the
+				 * latitude and longitude of the current location. Return at
+				 * most 1 address.
+				 */
+				addresses = geocoder.getFromLocation(location.getLatitude(),
+						location.getLongitude(), 1);
+				Log.d(LocationUtils.APPTAG,
+						"No of address = " + addresses.size());
+
+				// Catch network or other I/O problems.
+			} catch (IOException exception1) {
+
+				// Log an error and return an error message
+				Log.e(LocationUtils.APPTAG,
+						getString(R.string.IO_Exception_getFromLocation));
+
+				// print the stack trace
+				exception1.printStackTrace();
+
+				// Return an error message
+				return (getString(R.string.IO_Exception_getFromLocation));
+
+				// Catch incorrect latitude or longitude values
+			} catch (IllegalArgumentException exception2) {
+
+				// Construct a message containing the invalid arguments
+				String errorString = getString(
+						R.string.illegal_argument_exception,
+						location.getLatitude(), location.getLongitude());
+				// Log the error and print the stack trace
+				Log.e(LocationUtils.APPTAG, errorString);
+				exception2.printStackTrace();
+
+				//
+				return errorString;
+			}
+			// If the reverse geocode returned an address
+			if (addresses != null && addresses.size() > 0) {
+
+				// Get the first address
+				Address address = addresses.get(0);
+
+				// Format the first line of address
+				String addressText = getString(
+						R.string.address_output_string,
+
+						// If there's a street address, add it
+						address.getMaxAddressLineIndex() > 0 ? address
+								.getAddressLine(0) : "",
+
+						// Locality is usually a city
+						address.getLocality(),
+
+						// The country of the address
+						address.getCountryName());
+
+				Log.d(LocationUtils.APPTAG, addressText);
+
+				// Return the text
+				return addressText;
+
+				// If there aren't any addresses, post a message
+			} else {
+				return getString(R.string.no_address_found);
+			}
+		}
+
+		/**
+		 * A method that's called once doInBackground() completes. Set the text
+		 * of the UI element that displays the address. This method runs on the
+		 * UI thread.
+		 */
+		@Override
+		protected void onPostExecute(String address) {
+
+			// Set the address in the UI
+			mAddressBar.setText(address);
+		}
+	}
+
+}
